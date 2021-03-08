64acbd64969361c6a7e1b57ce1d3a46f912e33ad
Ryan Zidago <ryanzidago@gmail.com>
2021-03-08 21:36:17 +0100

    formatted json struct

--- config/config.go
+++ config/config.go
@@ -2,24 +2,24 @@ package config
 
 import (
 	"encoding/json"
-	"os"
 	"log"
+	"os"
 )
 
 // Config struct holds the application's configuration
 type Config struct {
-	HerokuAPIKey                   string   `json:herokuAPIKey`
-	HerokuAPIEndpoint              string   `json:herokuAPIEndpoint`
-	HerokuTeam                     string   `json:herokuTeam`
-	ContextName                    string   `json:contextName`
-	Envs                           []string `json:envs`
-	Buildpacks                     []string `json:buildpacks`
-	LogentriesAPIKey               string   `json:logentriesAPIKey`
-	LogentriesAPIEndpoint          string   `json:logentriesAPIEndpoint`
-	TeamLogsetKey                  string   `json:teamLogsetKey`
-	LogentriesHerokuLogStructureID string   `json:logentriesHerokuLogStructureID`
-	RollbarAPIEndpoint             string   `json:rollbarAPIEndpoint`
-	RollbarAccountAccessToken      string   `json:rollbarAccountAccessToken`
+	HerokuAPIKey                   string   `json:"herokuAPIKey"`
+	HerokuAPIEndpoint              string   `json:"herokuAPIEndpoint"`
+	HerokuTeam                     string   `json:"herokuTeam"`
+	ContextName                    string   `json:"contextName"`
+	Envs                           []string `json:"envs"`
+	Buildpacks                     []string `json:"buildpacks"`
+	LogentriesAPIKey               string   `json:"logentriesAPIKey"`
+	LogentriesAPIEndpoint          string   `json:"logentriesAPIEndpoint"`
+	TeamLogsetKey                  string   `json:"teamLogsetKey"`
+	LogentriesHerokuLogStructureID string   `json:"logentriesHerokuLogStructureID"`
+	RollbarAPIEndpoint             string   `json:"rollbarAPIEndpoint"`
+	RollbarAccountAccessToken      string   `json:"rollbarAccountAccessToken"`
 }
 
 // Reader is used by other modules to read the application's configuration
